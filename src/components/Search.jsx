bd70fc7c95f0a8e655e518190d4fbb3abfb2ce92
ere99 <pollayen@gmail.com>
2024-09-20 08:33:21 +0100

    first commit

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,62 +1,61 @@
-import React from 'react'
+import React from "react";
 import {
-    Select,
-    SelectContent,
-    SelectItem,
-    SelectTrigger,
-    SelectValue,
-  } from "@/components/ui/select"
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
 
-import { Separator } from "@/components/ui/separator"
+import { Separator } from "@/components/ui/separator";
 import { CiSearch } from "react-icons/ci";
-import Data from '@/Shared/Data';
-
-  
+import Data from "@/Shared/Data";
 
 const Search = () => {
   return (
-    <div className='p-2 pd-5 bg-white rounded-md md:rounded-full flex-col 
-    md:flex md:flex-row gap-10 px-5 items-center w-[60%] '>
-        <Select>
+    <div
+      className="p-2 pd-5 bg-white rounded-md md:rounded-full flex-col 
+    md:flex md:flex-row gap-10 px-5 items-center w-[60%] "
+    >
+      <Select>
         <SelectTrigger className="outline-none border-none w-full shadow-none text-lg">
-            <SelectValue placeholder="Condition" />
+          <SelectValue placeholder="Condition" />
         </SelectTrigger>
         <SelectContent>
-            <SelectItem value="light">New</SelectItem>
-            <SelectItem value="dark">Old</SelectItem>
-            
+          <SelectItem value="light">New</SelectItem>
+          <SelectItem value="dark">Old</SelectItem>
         </SelectContent>
-        </Select>
-        
-        <Separator orientation="vertical" className= "hidden md:block"/>
+      </Select>
 
-        <Select>
+      <Separator orientation="vertical" className="hidden md:block" />
+
+      <Select>
         <SelectTrigger className="outline-none border-none w-full shadow-none text-lg">
-            <SelectValue placeholder=" Instrument Types" />
+          <SelectValue placeholder=" Instrument Types" />
         </SelectTrigger>
         <SelectContent>
-            {Data.instType.map((Instrument, index)=> (<SelectItem value={Instrument.name}>{Instrument.name}</SelectItem>))}
-            
+          {Data.instType.map((Instrument, index) => (
+            <SelectItem value={Instrument.name}>{Instrument.name}</SelectItem>
+          ))}
         </SelectContent>
-        </Select>
-        <Separator orientation="vertical " className="hidden md:block"/>
+      </Select>
+      <Separator orientation="vertical " className="hidden md:block" />
 
-
-        <Select>
+      <Select>
         <SelectTrigger className="outline-none border-none w-full shadow-none text-lg">
-            <SelectValue placeholder="Pricing" />
+          <SelectValue placeholder="Pricing" />
         </SelectTrigger>
         <SelectContent>
-            {Data.Pricing.map((Prices, index)=> (<SelectItem value={Prices.amount}>{Prices.amount}</SelectItem>))}
-            
-           
+          {Data.Pricing.map((Prices, index) => (
+            <SelectItem value={Prices.amount}>{Prices.amount}</SelectItem>
+          ))}
         </SelectContent>
-        </Select>
-        <div>
-        <CiSearch  className='bg-primary rounded-full p-4 text-[50px] text-white hover:scale-105 transition-all cursor-pointer' />
-        </div>
+      </Select>
+      <div>
+        <CiSearch className="bg-primary rounded-full p-4 text-[50px] text-white hover:scale-105 transition-all cursor-pointer" />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Search
+export default Search;
